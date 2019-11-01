7de3d5eee2fb12ad35d1a40c141a68132bacfebb
Celeste Lipford <clipford.com>
2019-11-01 18:54:58 -0400

    try adding debug statements to see what is happening

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+git status
+ls
 docker build -t clipford7/multi-client:latest -t clipford7/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t clipford7/multi-server:latest -t clipford7/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t clipford7/multi-worker:latest -t clipford7/multi-worker:$SHA -f ./worker/Dockerfile ./worker
